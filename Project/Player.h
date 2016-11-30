8f8dd4b340fcf7a5ae1ce329f89d653a597a7918
Josiah Coad <josiahcoad@build.tamu.edu>
2016-11-30 11:44:22 -0600

    commented Players.h

--- Project/Player.h
+++ Project/Player.h
@@ -1,20 +1,34 @@
+/*
+ HNR CSCE 121-200, Fall 2016, Final Project
+ Author: Megan Grahmann, Clare Lamers, Josiah Coad
+ CODE DESCRIPTION: This code gives backend data management
+ 						 for the list of players.
+*/
+
+// "lowest level" h file... encapsulated by the PlayerList
 #include "std_lib_facilities_4.h"
 
+// import guards
 #ifndef PLAYER_H
 #define PLAYER_H
 
 struct Player{
-	Player(){} // allows an empty initialization of this class
-	Player(string name,
-	 		 string picture,
-	 		 vector<int> scores){
+	// constructor allows an empty initialization of this class
+	Player(){} 
+	// constructor for making a new player with player info
+	Player(string name,     // player name
+	 		 string picture,  // picture name incl extension
+	 		 vector<int> scores) // a vector of existing scores
+	// body of constructor
+	{
 		this -> name = name;
 		this -> picture = picture;
 		this -> scores = scores;
 		topscore = 0;
-		set_topscore();
+		set_topscore(); 
 	}
-	// convenience functions 
+	
+	// get/set functions for data members
 	void set_name(string name){ this -> name = name; }
 	void set_picture(string picture){ this -> picture = picture; }
 	string get_name() { return name; }
@@ -23,7 +37,8 @@ struct Player{
 	int get_topscore() { return topscore; }
 	vector<int> get_scores() { return scores; }
 
-	// profile picture folder
+	// string of profile picture folder... 
+	// picture name gets concatenated to this 
 	string picfolder = "./profile_pics/";
 
 	// used to add a score to a player
@@ -33,12 +48,15 @@ struct Player{
 	}
 
 	private:
+	// sets member variable "topscore" from 
+	// the players vector of existing scores
 	void set_topscore(){
 		for (int s : scores)
 			if (s > topscore)
 				topscore = s;
 	}
 
+	// private member variables accessed through get/set
 	string name, picture;
 	int topscore;
 	vector<int> scores;
